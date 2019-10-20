85b5f87c052bf864c1508b6c78037469f676c1b3
damienhunter30 <damien.hunter9@gmail.com>
2019-10-20 20:15:47 +0100

    Code Update and added a Jar

--- ShortLongURL/src/UrlEpand/ShortUrl.java
+++ ShortLongURL/src/UrlEpand/ShortUrl.java
@@ -3,25 +3,36 @@ package UrlEpand;
 import java.io.*;
 import java.net.*;
 
+/**
+ * 
+ * @author Damien Hunter
+ * 
+ * Credit to Thomas Tasche, https://gist.github.com/TomTasche/1104272
+ *
+ * Short URL expands tiny URLs to full URLs. The reason for this app is 
+ * to ensure links are safe to click and follow.
+ *
+ */
+
 public class ShortUrl{
 	public static String expand(String url){
 		try{
-			// Gets the URL returned from reading the tiny/short url passed.
+			// Gets the URL returned from reading the tiny/short URL passed.
 			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
 			connection.setInstanceFollowRedirects(true);
 			connection.getInputStream().read();
 			
-			// Clean up the returned URL
+			// Clean up the returned URL and return as a string.
 			url = connection.toString().replace("sun.net.www.protocol.https.DelegateHttpsURLConnection:", "");
 		}catch(MalformedURLException e){
 			e.printStackTrace();
-			return null;
+			return "Please check the URL.";
 		}catch(IOException e){
 			e.printStackTrace();
-			return null;
+			return "Please check your connection.";
 		}catch(Exception e){
 			e.printStackTrace();
-			return null;
+			return "General Error.";
 		}
 		return url;
 	}
