5bc8b4b5fe7c2a011d953ecfce8fe1a9a8bca61d
damienhunter30 <damien.hunter9@gmail.com>
2019-10-20 16:02:36 +0100

    modified:   ShortLongURL/src/UrlEpand/ShortUrl.java

--- ShortLongURL/src/UrlEpand/ShortUrl.java
+++ ShortLongURL/src/UrlEpand/ShortUrl.java
@@ -1,10 +1,29 @@
 package UrlEpand;
 
-public class ShortUrl {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ShortUrl{
 
+	public static void expand(String url){
+		try{
+			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setInstanceFollowRedirects(true);
+			connection.getInputStream().read();
+			
+			url = connection.toString();
+			System.out.println(url);
+		}catch(MalformedURLException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		//return null;
 	}
-
+	
+	public static void main(String[] args){
+		ShortUrl.expand("https://tinyurl.com/2tx");
+	}
 }
